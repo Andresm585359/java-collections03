0edacedcfa5d26a3d8042fa6c9f1a2d8b0dd1a5c
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 20:54:46 -0300

    ta pronto n

---
+++ src/br/fai/collections03/Animal.java
@@ -0,0 +1,41 @@
+package br.fai.collections03;
+
+public abstract class Animal {
+	
+	public void som() {
+		System.out.println("Este é o som padrão do animal:  ");
+	}
+	
+	public void dizerMeuNome() {
+		System.out.println("O nome do animal é: ");
+	}
+	
+	private void dizerMinhaIdade() {
+		System.out.println("Minha idade é: ");
+	}
+	
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	private String cor = "preto";
+	private int idade = 5;
+	private String nome = "Não definido";
+
+}
