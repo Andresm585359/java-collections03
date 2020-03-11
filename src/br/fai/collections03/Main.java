e0e8af40733b58cd04c261c855157b7c4a79f159
Matuk <mateusmatuk@fai-mg.br>
2020-03-11 20:41:09 -0300

    primeiro commite de venda de drogas

---
+++ src/br/fai/collections03/Main.java
@@ -0,0 +1,18 @@
+package br.fai.collections03;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Main app = new Main();
+		app.Start();
+
+	}
+
+	private void Start() {
+		
+		System.out.println("ueh");
+		
+	}
+
+}
